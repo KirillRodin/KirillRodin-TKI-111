6e1a2de52ab9028eb5aebbc8225b783434cfacff
K1rill1 <91265868+KirillRodin@users.noreply.github.com>
2021-11-21 16:58:47 +0000

    Задание 1-2

--- 1-2.cpp
+++ 1-2.cpp
@@ -1,26 +1,13 @@
 #define _USE_MATH_DEFINES
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-/**
-* \brief Математическая функция, рассчитывающая площадь прямоугольника
-* \param const double a - длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
-* \param const double n - коэффициент, имеющий тип данных с плавающей точкой двойной точности
-* \return Возвращает площадь чисел
-**/
-double getS(const double a, const double n);
+double getSquare(const double a, const double n);
 
-/**
-* \brief Математическая функция, рассчитывающая периметр прямоугольника
-* \param const double a - длина прямоугольника, имеющая тип данных с плавающей точкой двойной точности
-* \param const double n - коэффициент, имеющий тип данных с плавающей точкой двойной точности
-* \return Возвращает периметр прямоугольника 
-**/
-double getP(const double a, const double n);
-
+double getPerimeter(const double a, const double n);
 
 /**
  * \brief Точка входа в программу.
@@ -28,16 +15,13 @@ double getP(const double a, const double n);
  */
 int main() {
   double a, n;
-
   cout << "Введите длину и коэффициент и нажмите Enter: \n";
   cin >> a >> n;
-
-	const auto S = getS(a, n);
-	const auto P = getP(a, n);
-
-	cout << "Стророна a = " << a << "\nКоэффициент n = " << n << "\nПериметр = " << S << "\nПлощадь = " << P;
+	const auto square = getSquare(a, n);
+	const auto perimeter = getPerimeter(a, n);
+	cout << "Стророна a = " << a << "\nКоэффициент n = " << n << "\nПериметр = " << square << "\nПлощадь = " << perimeter;
 	return 0;
 }
 
-double getS(const double a, const double n) { return pow(a, 2) * n; } 
-double getP(const double a, const double n) { return 2 * a * (1 + n); } 
+double getSquare(const double a, const double n) { return pow(a, 2) * n; } 
+double getPerimeter(const double a, const double n) { return 2 * a * (1 + n); } 

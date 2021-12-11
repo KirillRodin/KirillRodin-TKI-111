e69571a9a1af8f502aa566858f1d664ee3d9be68
K1rill1 <91265868+KirillRodin@users.noreply.github.com>
2021-12-11 22:18:10 +0000

    Задание 1-2

--- 1-2.cpp
+++ 1-2.cpp
@@ -26,14 +26,15 @@ double getPerimeter(const double a, const double n);
  * \return Возвращает 0 в случае успешного выполнения.
  */
 int main() {
-  double a, n;
-  cout << "Введите длину и коэффициент и нажмите Enter: \n";
-  cin >> a >> n;
+	setlocale(LC_ALL, "Russian");
+	double a, n;
+	cout << "Введите длину и коэффициент и нажмите Enter: \n";
+	cin >> a >> n;
 	const auto square = getSquare(a, n);
 	const auto perimeter = getPerimeter(a, n);
-	cout << "Стророна a = " << a << "\nКоэффициент n = " << n << "\nПериметр = " << square << "\nПлощадь = " << perimeter;
+	cout << "Стророна a = " << a << "\nКоэффициент n = " << n << "\nПериметр = " << perimeter << "\nПлощадь = " << square;
 	return 0;
 }
 
-double getSquare(const double a, const double n) { return pow(a, 2) * n; } 
-double getPerimeter(const double a, const double n) { return 2 * a * (1 + n); } 
+double getSquare(const double a, const double n) { return pow(a, 2) * n; }
+double getPerimeter(const double a, const double n) { return 2 * a * (1 + n); }
